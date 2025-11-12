c5d56d0b4e140d9d7f242f04b2e727311bc535e9
Emmanuel Olarewaju <emmanuelmark501@gmail.com>
2025-11-12 20:23:09 +0100

    factorial

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,10 +1,8 @@
 const { argv } = require("node:process");
 let k = Number(argv[2]);
 if (isNaN(k)) k = 1;
-function recursion(number) {
-  if (k == 0) return number;
-  number = number * k;
-  k = k - 1;
-  return recursion(number);
+function recursion(number, n) {
+  if (n == 0) return number;
+  return recursion(number * n, n - 1);
 }
-console.log(recursion(k));
+console.log(recursion(k, k));
